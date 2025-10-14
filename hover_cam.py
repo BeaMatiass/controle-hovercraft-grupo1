2a8d7723f3831c60025535663c018c62fa706eef
phabreuadorno308 <phabreuadorno@usp.br>
2025-10-14 16:14:30 -0300

    Últimas atualizações. Rodrigo elogiou a lógica usada. Trabalhei melhorando a dinâmica dos tópicos, alterei seus nomes e adicionei um algoritmo para resolver uma situação que não tínhamos previsto: o hover não receber nenhum dado visual. Caso permanecesse do jeito anterior, ele iria ficar parado e não iria girar no sentido angular. Alterei isso

--- hover_cam.py
+++ hover_cam.py
@@ -3,26 +3,28 @@
 import rospy
 import cv2
 from sensor_msgs.msg import Image
-from std_msgs.msg import String
+from std_msgs.msg import Float32
 from cv_bridge import CvBridge
 
 bridge = CvBridge()
 contador = 0
 
 def funcao_callback(image):
-
+    # Buffer
     global contador
     contador += 1
 
     if(contador % 6 != 0):
         return
     
+    # Conversão
     try:
         frame = bridge.imgmsg_to_cv2(image, desired_encoding='bgr8')
     except Exception as e:
         rospy.logerr(f"Erro ao converter imagem: {e}")
         return
 
+    # Algoritmo reconhecimento pixels vermelhos
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask1 = cv2.inRange(hsv, (0, 150, 50), (10, 255, 255))
     mask2 = cv2.inRange(hsv, (160, 150, 50), (179, 255, 255))
@@ -33,32 +35,25 @@ def funcao_callback(image):
     min_pixels = height*width*0.135
     num_pixels = cv2.countNonZero(mask)
     
-
+    # Se houver objeto vermelho na tela, publica suas informações
     if(num_pixels > min_pixels): 
         
         cx = int(M['m10']/M['m00'])
-        cy = int(M['m01']/M['m00'])
-
         dx = cx - (width // 2)
         tamanho_atual = num_pixels / (height * width)
 
-        msg = f"{dx},{tamanho_atual}"
-        pub_world.publish(msg) # publisher em /world -> alterar depois para publicar em subtópicos as três informações separadamente
+        pub_distancia_x.publish(dx)
+        pub_tamanho_atual.publish(tamanho_atual)
 
-        cv2.circle(frame, (cx, cy), 30, (0, 0, 255), -1)
-        cv2.line(frame, (cx, cy), (width // 2, height // 2), (0, 0, 255), 5)
-
-    cv2.imshow('Video', frame)
-    cv2.waitKey(1)
 
 if __name__ == '__main__':
     rospy.init_node("camera_gazebo")
 
-    pub_world = rospy.Publisher("/world", String, queue_size=1)
+    # Publishers
+    pub_distancia_x = rospy.Publisher("/dados_da_camera/distancia_x", Float32, queue_size=1)
+    pub_tamanho_atual = rospy.Publisher("/dados_da_camera/tamanho_atual", Float32, queue_size=1)
+
+    # Subscribers
     rospy.Subscriber("camera/rgb/image_raw", Image, funcao_callback, queue_size=1)
 
-    while not rospy.is_shutdown():
-        pass
-
-    cv2.destroyAllWindows()
-
+    rospy.spin()
--- hover_navigate.py
+++ hover_navigate.py
@@ -1,90 +1,88 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import String
-from geometry_msgs.msg import Twist
-import time
+from std_msgs.msg import Float32
+from codigo_pid import controle_pid
 
-def processar_pid(erro, estado, kp, ki, kd):
+# Objetos globais
+pub_speed = None
+pub_angular = None
+pub_linear = None
 
-    # tempo -> calculo derivativo e integral
-    tempo_atual = time.time()
+# Variáveis globais
+dx = None
+tamanho_atual = None
+tamanho_capturado = 0.60
 
-    # primeira interação não se calcula integral e derivativo, só proporcional -> dt = 0.0
-    if estado["tempo_antigo"] == 0.0:
-        dt = 0.0
-        estado["tempo_antigo"] = tempo_atual
-    else:
-        dt = tempo_atual - estado["tempo_antigo"]
-        estado["tempo_antigo"] = tempo_atual
-    
-    # acumula o erro
-    estado["integral"] += erro * dt
+ultimo_tempo_visto = None
 
-    # calcula ação derivativa
-    if dt > 0:
-        acao_derivativa = (erro - estado["erro_antigo"]) / dt
-    else:
-        acao_derivativa = 0.0
-    
-    # calcula ação de controle total
-    acao_de_controle = kp * erro + ki * estado["integral"] + kd * acao_derivativa
+def callback_dx(msg):
+    global dx, ultimo_tempo_visto
+    dx = msg.data
+    ultimo_tempo_visto = rospy.Time.now()
+    processar_controle()
+    return
 
-    # define como erro_antigo o erro já computado
-    estado["erro_antigo"] = erro
-
-    return acao_de_controle
+def callback_tamanho(msg):
+    global tamanho_atual, ultimo_tempo_visto
+    tamanho_atual = msg.data
+    ultimo_tempo_visto = rospy.Time.now()
+    processar_controle()
+    return
 
 
+def processar_controle():
+    global dx, tamanho_atual
+    global pid_angular, pid_linear
+    global pub_linear, pub_angular
+    global tamanho_capturado
 
-def callback_world(msg):
-
-    global estado_angular, estado_linear, pub_speed
-    global kp_angular, ki_angular, kd_angular
-
-    lista = msg.split(",")
-
-    dx = float(lista[0])
-    tamanho_atual = float(lista[1])
-    tamanho_capturado = 0.60 # proporção desejada do objeto em relação a imagem
-
+    if dx is None or tamanho_atual is None:
+        return
 
     if tamanho_atual >= tamanho_capturado:
-        acao_de_controle_linear = 0.0
-        acao_de_controle_angular = 0.0
+        acao_linear = 0.0
+        acao_angular = 0.0
     else:
-        # PID para rotação (erro: dx)
-        acao_de_controle_angular = processar_pid(dx, estado_angular, kp_angular, ki_angular, kd_angular)
-        # PID para linear (erro: erro_distancia)
+        acao_angular = pid_angular.processar_pid(dx)
         erro_distancia = tamanho_capturado - tamanho_atual
-        acao_de_controle_linear = processar_pid(erro_distancia, estado_linear, kp_linear, ki_linear, kd_linear)
+        acao_linear = pid_linear.processar_pid(erro_distancia)
 
-    cmd = Twist()
-    cmd.linear.x = acao_de_controle_linear
-    cmd.angular.z = -acao_de_controle_angular
-    pub_speed.publish(cmd)
-
+    pub_linear.publish(acao_linear)
+    pub_angular.publish(-acao_angular)
     return
+
+def controle_periodico():
+    global ultimo_tempo_visto
+
+    if ultimo_tempo_visto is None:
+        pub_linear.publish(0.0)
+        pub_linear.publish(0.3)
+        return
+    
+    tempo_sem_objeto = (rospy.Time.now() - ultimo_tempo_visto)
+    if tempo_sem_objeto > 1.0:
+        pub_linear.publish(0.0)
+        pub_linear.publish(0.3)
+        return
+
  
 if __name__ == '__main__':
     rospy.init_node("hover_navigate")
 
-    # Estados do PID
-    estado_angular = {"integral": 0.0, "erro_antigo": 0.0, "tempo_antigo": 0.0}
-    estado_linear = {"integral": 0.0, "erro_antigo": 0.0, "tempo_antigo": 0.0}
+    # Inicializa o PID
+    pid_angular = controle_pid(kp = 0.002, ki = 0.001, kd = 0.001)
+    pid_linear = controle_pid(kp = 1.0, ki = 0.0, kd = 0.0)
 
-    # Parâmetros do PID - Rotação
-    kp_angular = 0.002 # ajustar
-    ki_angular = 0.001 # ajustar
-    kd_angular = 0.001 # ajustar
+    # Publishers
+    pub_linear = rospy.Publisher("/speed/velocidade_linear", Float32, queue_size=1)
+    pub_angular = rospy.Publisher("/speed/velocidade_angular", Float32, queue_size=1)
 
-    # Parâmetros do PID - Linear
-    kp_linear = 1.0 # ajustar
-    ki_linear = 0.0 # ajustar
-    kd_linear = 0.0 # ajustar
+    # Subscribers
+    rospy.Subscriber("/dados_da_camera/distancia_x", Float32, callback_dx)
+    rospy.Subscriber("/dados_da_camera/tamanho_atual", Float32, callback_tamanho)
 
-    pub_speed = rospy.Publisher("/speed", Twist, queue_size=1)
-    rospy.Subscriber("/world", String, callback_world) # mudar nome tópico
+    # Timer periódico (busca quando não há dados obtidos no subscriber -> sem objeto na tela)
+    rospy.Timer(rospy.Duration(0.1), controle_periodico)
 
-    while not rospy.is_shutdown():
-        pass
+    rospy.spin()
