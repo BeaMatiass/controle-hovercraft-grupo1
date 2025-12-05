133a7437ca7cff03afdadf26c77e8d81ea524087
Hugo <ayello.hugo@usp.br>
2025-12-05 00:39:22 -0300

    Matriz de distorção adicionada

--- camera.py
+++ camera.py
@@ -2,7 +2,7 @@
 
 import rospy
 import cv2
-import numpy as np  # Necessário para matrizes
+import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
@@ -18,7 +18,6 @@ K = np.array([
 D = np.array([0.080291, -0.138517, -0.002353, -0.002475, 0.000000])
 
 # Matriz de Projeção (Projection Matrix - P)
-# Usamos apenas a parte 3x3 (rotacional/escala) para o newCameraMatrix
 P = np.array([
     [581.062867, 0.000000, 260.924261],
     [0.000000, 582.487228, 257.382226],
@@ -28,16 +27,17 @@ P = np.array([
 def main():
     rospy.init_node("camera_webcam_pub")
 
-    pub = rospy.Publisher("/camera/rgb/image_rect_color", Image, queue_size=1) # Mudei o tópico para indicar que é retificada
+    pub = rospy.Publisher("/camera/rgb/image_raw", Image, queue_size=1)
+    
     bridge = CvBridge()
 
-    cap = cv2.VideoCapture(3) 
+    cap = cv2.VideoCapture(3)
 
     if not cap.isOpened():
         rospy.logerr("Não foi possível abrir a webcam!")
         return
 
-    rate = rospy.Rate(30) 
+    rate = rospy.Rate(30)
 
     while not rospy.is_shutdown():
         ret, frame = cap.read()
@@ -45,22 +45,13 @@ def main():
             rospy.logwarn("Falha ao capturar frame!")
             continue
 
-        # --- APLICA A CALIBRAÇÃO (UNDISTORT) ---
-        # frame: imagem original
-        # K: matriz original da câmera
-        # D: distorção
-        # None: retificação (não usada aqui pois é monocular simples)
-        # P: nova matriz de câmera (como a imagem deve ficar após o corte/zoom ideal)
+        # Aplica a calibração usando a matriz de projeção P para o corte ideal
         frame_rectified = cv2.undistort(frame, K, D, None, P)
         
-        # frame_rectified = cv2.undistort(frame, K, D)
-        # --- Mostra a imagem na tela (Comparação) ---
-        # Mostra a imagem corrigida
         cv2.imshow("Webcam Retificada", frame_rectified)
-        
         cv2.waitKey(1) 
 
-        # --- Publica no ROS a imagem JÁ CORRIGIDA ---
+        # Publica imagem retificada no ROS
         img_msg = bridge.cv2_to_imgmsg(frame_rectified, encoding="bgr8")
         pub.publish(img_msg)
 
